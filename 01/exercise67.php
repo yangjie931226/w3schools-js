04b7511b6d35c0b9b06b4489e911e2a60a6c790f
yangjie931226 <13512788604@163.com>
2017-07-04 14:04:57 +0800

    67

---
+++ 01/exercise67.php
@@ -0,0 +1,12 @@
+﻿<?php
+/**
+ * @author 杨杰 <yangjie@163.com>
+ * @version 1.0
+ * @copyright 2014-2017 海量云图（北京）数据技术有限公司
+ */
+
+$myObj->name = "John";
+$myObj->age = 30;
+$myObj->city = "New York";
+$myJSON = json_encode($myObj);
+echo $myJSON;
